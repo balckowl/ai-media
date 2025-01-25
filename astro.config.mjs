9df165b2a2032c273b8a44e17cb1e3771b1acfa3
AIM-KAmi0 <c5624012@ima.aim.aoyama.ac.jp>
2025-01-26 00:52:47 +0900

    fix: 個別記事の修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
   adapter: cloudflare(),
+  vite: {
+    plugins: [nodePolyfills()],
+    ssr: {
+      external: ['node:buffer', 'node:worker_threads', 'node:async_hooks', 'undici']
+    }
+  },
 });
--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -7,6 +7,9 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeToc from "rehype-toc";
 import { visit } from "unist-util-visit";
 import rehypeSlug from "rehype-slug";
+import ogs from "open-graph-scraper";
+import rehypePrettyCode from "rehype-pretty-code";
+
 // import ogs from "open-graph-scraper"
 // import ogs from "open-graph-scraper";
 
@@ -51,131 +54,133 @@ import rehypeSlug from "rehype-slug";
 //   };
 // }
 
-// function ogpLinkCardPlugin() {
-//   return async (tree: any) => {
-//     const promises: Promise<void>[] = [];
+function ogpLinkCardPlugin() {
+  return async (tree: any) => {
+    const promises: Promise<void>[] = [];
 
-//     visit(tree, 'element', (node, index: any, parent) => {
-//       if (
-//         node.tagName === 'a' && // リンクであることを確認
-//         node.properties?.href && // href属性があることを確認
-//         parent &&
-//         parent.tagName === 'p' && // 親が<p>タグ
-//         parent.children.length === 1 && // 子要素が1つだけ
-//         parent.children[0] === node // その子要素が現在のリンク
-//       ) {
-//         const url = node.properties.href as string;
+    visit(tree, 'element', (node, index: any, parent) => {
+      if (
+        node.tagName === 'a' && // リンクであることを確認
+        node.properties?.href && // href属性があることを確認
+        parent &&
+        parent.tagName === 'p' && // 親が<p>タグ
+        parent.children.length === 1 && // 子要素が1つだけ
+        parent.children[0] === node // その子要素が現在のリンク
+      ) {
+        const url = node.properties.href as string;
 
-//         // 非同期処理のPromiseを作成
-//         const promise = (async () => {
-//           try {
-//             const { result } = await ogs({ url });
+        // 非同期処理のPromiseを作成
+        const promise = (async () => {
+          try {
 
-//             const title = result.ogTitle || url;
-//             const imageUrl = result.ogImage && result.ogImage[0]?.url || '';
-//             const description = result.ogDescription || '';
-//             const domain = new URL(url).origin;
+            const { result } = await ogs({ url });
 
-//             // リンクカードノードを作成
-//             const linkCardNode = {
-//               type: 'element',
-//               tagName: 'div',
-//               properties: { className: ['link-card'] },
-//               children: [
-//                 {
-//                   type: 'element',
-//                   tagName: 'a',
-//                   properties: {
-//                     href: url,
-//                     target: '_blank',
-//                     rel: 'noopener noreferrer',
-//                     className: ['link-card-anchor'],
-//                   },
-//                   children: [
-//                     {
-//                       type: 'element',
-//                       tagName: 'div',
-//                       properties: { className: ['link-card-content'] },
-//                       children: [
-//                         imageUrl
-//                           ? {
-//                               type: 'element',
-//                               tagName: 'img',
-//                               properties: {
-//                                 src: imageUrl,
-//                                 alt: title,
-//                                 className: ['link-card-image'],
-//                               },
-//                             }
-//                           : null,
-//                         {
-//                           type: 'element',
-//                           tagName: 'div',
-//                           properties: { className: ['link-card-text'] },
-//                           children: [
-//                             {
-//                               type: 'element',
-//                               tagName: 'div',
-//                               properties: { className: ['link-card-text-wrapper'] }, // title と description をまとめる <div>
-//                               children: [
-//                                 {
-//                                   type: 'element',
-//                                   tagName: 'p', // タイトルを <h3> でラップ
-//                                   properties: { className: ['link-card-title'] },
-//                                   children: [
-//                                     { type: 'text', value: title },
-//                                   ],
-//                                 },
-//                                 description
-//                                   ? {
-//                                       type: 'element',
-//                                       tagName: 'p', // 説明を <p> でラップ
-//                                       properties: {
-//                                         className: ['link-card-description'],
-//                                       },
-//                                       children: [
-//                                         { type: 'text', value: description },
-//                                       ],
-//                                     }
-//                                   : null,
-//                               ].filter(Boolean),
-//                             },
-//                             {
-//                               type: 'element',
-//                               tagName: 'p',
-//                               properties: {
-//                                 className: ['link-card-link'],
-//                               },
-//                               children: [
-//                                 { type: 'text', value: domain },
-//                               ],
-//                             },
-//                           ].filter(Boolean),
-//                         },
-//                       ].filter(Boolean),
-//                     },
-//                   ],
-//                 },
-//               ],
-//             };
+            const title = result.ogTitle || url;
+            const imageUrl = result.ogImage && result.ogImage[0]?.url || '';
+            const description = result.ogDescription || '';
+            console.log(description);
+            const domain = new URL(url).origin;
 
-//             // 親ノード全体をリンクカードノードに置き換え
-//             parent.tagName = 'div';
-//             parent.properties = { className: ['link-card-wrapper'] };
-//             parent.children = [linkCardNode];
-//           } catch (error) {
-//             console.error(`Failed to fetch OGP for ${url}:`, error);
-//           }
-//         })();
+            // リンクカードノードを作成
+            const linkCardNode = {
+              type: 'element',
+              tagName: 'div',
+              properties: { className: ['link-card'] },
+              children: [
+                {
+                  type: 'element',
+                  tagName: 'a',
+                  properties: {
+                    href: url,
+                    target: '_blank',
+                    rel: 'noopener noreferrer',
+                    className: ['link-card-anchor'],
+                  },
+                  children: [
+                    {
+                      type: 'element',
+                      tagName: 'div',
+                      properties: { className: ['link-card-content'] },
+                      children: [
+                        imageUrl
+                          ? {
+                            type: 'element',
+                            tagName: 'img',
+                            properties: {
+                              src: imageUrl,
+                              alt: title,
+                              className: ['link-card-image'],
+                            },
+                          }
+                          : null,
+                        {
+                          type: 'element',
+                          tagName: 'div',
+                          properties: { className: ['link-card-text'] },
+                          children: [
+                            {
+                              type: 'element',
+                              tagName: 'div',
+                              properties: { className: ['link-card-text-wrapper'] }, // title と description をまとめる <div>
+                              children: [
+                                {
+                                  type: 'element',
+                                  tagName: 'p', // タイトルを <h3> でラップ
+                                  properties: { className: ['link-card-title'] },
+                                  children: [
+                                    { type: 'text', value: title },
+                                  ],
+                                },
+                                description
+                                  ? {
+                                    type: 'element',
+                                    tagName: 'p', // 説明を <p> でラップ
+                                    properties: {
+                                      className: ['link-card-description'],
+                                    },
+                                    children: [
+                                      { type: 'text', value: description },
+                                    ],
+                                  }
+                                  : null,
+                              ].filter(Boolean),
+                            },
+                            {
+                              type: 'element',
+                              tagName: 'p',
+                              properties: {
+                                className: ['link-card-link'],
+                              },
+                              children: [
+                                { type: 'text', value: domain },
+                              ],
+                            },
+                          ].filter(Boolean),
+                        },
+                      ].filter(Boolean),
+                    },
+                  ],
+                },
+              ],
+            };
 
-//         promises.push(promise);
-//       }
-//     });
+            // 親ノード全体をリンクカードノードに置き換え
+            parent.tagName = 'div';
+            parent.properties = { className: ['link-card-wrapper'] };
+            parent.children = [linkCardNode];
+          } catch (error) {
+            console.error(`Failed to fetch OGP for ${url}:`, error);
+          }
+        })();
 
-//     // すべての非同期処理が完了するのを待つ
-//     await Promise.all(promises);
-//   };
-// }
+        promises.push(promise);
+      }
+    });
+
+    // すべての非同期処理が完了するのを待つ
+    await Promise.all(promises);
+  };
+}
 
 
 export async function markdownToHtml(markdown: string): Promise<string> {
@@ -183,8 +188,12 @@ export async function markdownToHtml(markdown: string): Promise<string> {
     .use(remarkParse)
     .use(remarkRehype)
     .use(rehypeSlug)
-    .use(rehypeHighlight, { detect: true })
-    // .use(ogpLinkCardPlugin)
+    // .use(rehypeHighlight, { detect: true })
+    .use(rehypePrettyCode, {
+      theme: "github-dark",
+      keepBackground: true,
+    })
+    .use(ogpLinkCardPlugin)
     // .use(rehypeAutolinkHeadings, {
     //   behavior: 'prepend', content: {
     //     type: 'element',
